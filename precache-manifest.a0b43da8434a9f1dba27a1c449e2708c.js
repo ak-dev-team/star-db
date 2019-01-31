403bfcffbf49d5e7e10ace204e47f7205178dd8d
Andrii Kalashnikov <Andrii.Kalashnikov@global-e.com>
2019-01-31 10:54:39 +0200

    Updates

---
+++ precache-manifest.a0b43da8434a9f1dba27a1c449e2708c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-db/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "efac5df8a606d470b452",
+    "url": "/star-db/static/js/runtime~main.efac5df8.js"
+  },
+  {
+    "revision": "36affc896506d462423f",
+    "url": "/star-db/static/js/main.36affc89.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/star-db/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "36affc896506d462423f",
+    "url": "/star-db/static/css/main.d0e077a9.chunk.css"
+  },
+  {
+    "revision": "df2682ed4d1cc897b16ce1f2f12201f0",
+    "url": "/star-db/index.html"
+  }
+];
