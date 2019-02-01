2f3a9138c3520d0278c2a01c848e68b293f97e28
Andrii Kalashnikov <Andrii.Kalashnikov@global-e.com>
2019-02-01 13:27:46 +0200

    structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
-ReactDOM.render(<p>ffff</p>, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
